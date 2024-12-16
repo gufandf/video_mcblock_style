c32c34881733ea2861eb2c30df9f1bff88039c05
孤帆gufandf <465250060@qq.com>
2024-12-16 18:39:21 +0800

    remove unused file

--- trans.py
+++ trans.py
@@ -43,8 +43,8 @@ def transPhoto(input_path:str,output_path:str):
 
     for i in range(im.size[0]):
         for j in range(im.size[1]):
-            block = get_block(im.getpixel((i,j)))
-            # block = get_block_fast(im.getpixel((i,j)))
+            # block = get_block(im.getpixel((i,j)))
+            block = get_block_fast(im.getpixel((i,j)))
             blockim = Image.open(f"./block/{block}.png").copy().convert("RGBA")
             re.paste(blockim,[i*16,j*16])
 
@@ -58,7 +58,6 @@ def transVideo(input_path:str,output_path:str):
     print("[TRANS MAIN]正在拆分帧")
     videoCapture=cv2.VideoCapture(input_path)
     fps = videoCapture.get(cv2.CAP_PROP_FPS)
-
     subprocess.call(f"ffmpeg -loglevel quiet -i \"{input_path}\" -vf \"scale=iw/16:ih/16\" \"./temp/frame%06d.png\"")
     
     MAX_THREADS = 15
